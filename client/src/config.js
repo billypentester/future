f586a5a37f2b654ef41ffee7d9585800d86a8929
Bilal Ahmad <bilalsheikh2500@gmail.com>
2024-04-03 23:00:44 +0500

    urls updated for web app

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:5000/api';
-const imageURL = 'http://localhost:5000/uploads/';
+const baseURL = 'https://future-kohl.vercel.app/api';
+const imageURL = 'https://future-kohl.vercel.app/uploads/';
 
 export default axios.create({
     baseURL: baseURL,
